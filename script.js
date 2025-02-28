f288705cc98c4e5deb3f6039806812cef0046234
Shammi-Akther <shammisumi137@gmail.com.>
2025-03-01 04:20:49 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function clickChangeBg() {
+
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let rgbValue = "#" + randomColor;
+    document.body.style.backgroundColor = rgbValue;
+}
