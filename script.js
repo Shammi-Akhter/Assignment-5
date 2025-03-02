ba67f54bb64762f8f55024626b33fada474e2650
Shammi-Akther <shammisumi137@gmail.com.>
2025-03-02 14:28:58 +0600

    Adding Check Final

--- script.js
+++ script.js
@@ -4,51 +4,62 @@ function clickChangeBg() {
     let rgbValue = "#" + randomColor;
     document.body.style.backgroundColor = rgbValue;
 }
+   
+       
+        const currentDate = new Date();
 
+        
+        const dayOfWeek = currentDate.toLocaleString('en-US', { weekday: 'short' }); 
+        const month = currentDate.toLocaleString('en-US', { month: 'short' }); 
+        const day = currentDate.getDate(); 
+        const year = currentDate.getFullYear(); 
 
+        const formattedDate = `${dayOfWeek},`;
+        const date = `${month} ${day} ${year}`;
+
+        document.getElementById('current-day').textContent = ` ${formattedDate}`;
+        document.getElementById('current-date').textContent = ` ${date}`;
+     
 const buttons = document.querySelectorAll('.card button')
-    buttons.forEach((button) => {
-        button.addEventListener('click', function () {
+buttons.forEach((button) => {
+    button.addEventListener('click', function () {
 
-            if (button.disabled) return;
-            button.disabled = true;
-            button.style.backgroundColor = "gray";
-            button.style.cursor = "not-allowed";
-            alert("Board Updated Successfully");
-
-
-            let addElement = document.getElementById("checkBox-count");
-            addElement.textContent = parseInt(addElement.textContent) + 1;
-
-            let subElement = document.getElementById("task-count");
-            subElement.textContent = parseInt(subElement.textContent) - 1;
-            
-            let taskAssign = addElement.textContent;
-            if(taskAssign == 6)
-                {                   
-                        alert("Congrats!!!! You have completed all the current tasks!");
-                    
-                }
+        if (button.disabled) return;
+        button.disabled = true;
+        button.style.backgroundColor = "gray";
+        button.style.cursor = "not-allowed";
+        alert("Board Updated Successfully");
 
 
-            const taskTitle = button.closest(".card").querySelectorAll(".card-title").innerText;
-            const dateTimeString = new Date().toLocaleTimeString();
-        
-            const logEntry = document.createElement("p");
-            logEntry.classList.add("log");
-            logEntry.innerText = `You have Complete ${taskTitle} at ${dateTimeString}`;
-        
-            const activityLog = document.getElementById("log-container");
-            activityLog.appendChild(logEntry);
+        let addElement = document.getElementById("checkBox-count");
+        addElement.textContent = parseInt(addElement.textContent) + 1;
 
-            const clearHistory = document.getElementById("clear-history").addEventListener("click", function(){
-                const logContainer = document.getElementById('log-container');
-                logContainer.innerHTML = '';
+        let subElement = document.getElementById("task-count");
+        subElement.textContent = parseInt(subElement.textContent) - 1;
 
-            })
+        let taskAssign = subElement.textContent;
+        if (taskAssign == 0) {
+            alert("Congrats!!!! You have completed all the current tasks!");
 
-        });
+        }
+
+        const taskTitle = button.closest(".card-div").querySelector(".card-title").innerText;
+        const dateTimeString = new Date().toLocaleTimeString();
+       
+        const logEntry = document.createElement("p");
+        logEntry.classList.add("log");
+        logEntry.innerText = `You have Complete ${taskTitle} at ${dateTimeString}`;
+
+        const activityLog = document.getElementById("log-container");
+        activityLog.appendChild(logEntry);
+
+        document.getElementById("clear-history").addEventListener("click", function () {
+            const logContainer = document.getElementById('log-container');
+            logContainer.innerHTML = '';
+
+        })
+
     });
+});
 
 
-    
